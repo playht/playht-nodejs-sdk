5656ac36785d4ddfe57d4d6e34ed9e71c03f8c87
Rodrigo Silveira <rodrigo@play.ht>
2023-09-04 16:04:32 +0100

    1 esconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json'],
+    project: ['./tsconfig.json', './example/tsconfig.json'],
   },
   plugins: ['@typescript-eslint'],
   rules: {
