945a26e1c3d34c12af977eca2f161db6ce7d018f
Rodrigo Silveira <rodrigo@play.ht>
2023-09-05 15:43:09 +0100

    Use cloned voice

--- example/src/streamSpeech.ts
+++ example/src/streamSpeech.ts
@@ -3,14 +3,19 @@ import * as PlayHTAPI from '../../dist';
 
 export async function streamSpeech(req: Request, res: Response, next: NextFunction) {
   const { text } = req.query;
-  const voice = req.body.voiceId || 'florencio';
-  const voiceEngine = req.body.VoiceEngine || 'PlayHT1.0';
+  const voice = req.query.voiceId || 'florencio';
+  const voiceEngine = req.query.VoiceEngine || 'PlayHT1.0';
 
   if (!text || typeof text !== 'string') {
     res.status(400).send('Text to generate not provided in the request');
     return next();
   }
 
+  if (typeof voice !== 'string' || (voiceEngine !== 'PlayHT1.0' && voiceEngine !== 'Standard')) {
+    res.status(400).send('Invalid voice params');
+    return next();
+  }
+  
   res.setHeader('Content-Type', 'audio/mpeg');
   try {
     // Call the API
