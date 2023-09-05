2875f38d69122ab759e3c217a2a336f77a9d533f
Rodrigo Silveira <rodrigo@play.ht>
2023-09-05 15:11:20 +0100

    Add voice variables to example

--- example/src/streamSpeech.ts
+++ example/src/streamSpeech.ts
@@ -3,6 +3,8 @@ import * as PlayHTAPI from '../../dist';
 
 export async function streamSpeech(req: Request, res: Response, next: NextFunction) {
   const { text } = req.query;
+  const voice = req.body.voiceId || 'florencio';
+  const voiceEngine = req.body.VoiceEngine || 'PlayHT1.0';
 
   if (!text || typeof text !== 'string') {
     res.status(400).send('Text to generate not provided in the request');
@@ -12,7 +14,7 @@ export async function streamSpeech(req: Request, res: Response, next: NextFuncti
   res.setHeader('Content-Type', 'audio/mpeg');
   try {
     // Call the API
-    await PlayHTAPI.streamSpeech(text, res);
+    await PlayHTAPI.streamSpeech(text, res, { voiceEngine: voiceEngine, voiceId: voice });
   } catch (error: any) {
     res.statusMessage = error?.message;
     res.status(error?.status || 500).send();
--- example/src/textToSpeech.ts
+++ example/src/textToSpeech.ts
@@ -8,11 +8,13 @@ export async function textToSpeech(req: Request, res: Response, next: NextFuncti
   }
 
   const text = req.body.text;
+  const voice = req.body.voiceId || 'florencio';
+  const voiceEngine = req.body.VoiceEngine || 'PlayHT1.0';
 
   res.set('Content-Type', 'application/json');
   try {
     // Call the API
-    const generated = await PlayHTAPI.generateSpeech(text);
+    const generated = await PlayHTAPI.generateSpeech(text, { voiceEngine: voiceEngine, voiceId: voice });
 
     res.status(200).json(generated);
   } catch (error: any) {
