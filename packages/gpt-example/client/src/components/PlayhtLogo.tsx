63e33999b1c601f8326d58e0200d15939103a845
Rodrigo Silveira <rodrigo@play.ht>
2023-09-20 18:42:11 +0100

    GPT example UI update

--- packages/gpt-example/client/src/components/PlayhtLogo.tsx
+++ packages/gpt-example/client/src/components/PlayhtLogo.tsx
@@ -1,9 +1,5 @@
 import PlayHTLogo from '../assets/playht-logo-dark.svg';
 
 export function PlayhtLogo() {
-  return (
-    <>
-      <img src={PlayHTLogo} className="block pb-12" alt="PlayHT Logo" />
-    </>
-  );
+  return <img src={PlayHTLogo} className="block pb-12 w-32 sm:w-40" alt="PlayHT Logo" />;
 }
