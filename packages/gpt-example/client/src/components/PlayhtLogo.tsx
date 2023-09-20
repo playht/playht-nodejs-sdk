b89e5a75aa4e091532d9d08973bafaff4b056bcc
Rodrigo Silveira <rodrigo@play.ht>
2023-09-20 16:12:06 +0100

    Client UI changes

---
+++ packages/gpt-example/client/src/components/PlayhtLogo.tsx
@@ -0,0 +1,9 @@
+import PlayHTLogo from '../assets/playht-logo-dark.svg';
+
+export function PlayhtLogo() {
+  return (
+    <>
+      <img src={PlayHTLogo} className="block pb-12" alt="PlayHT Logo" />
+    </>
+  );
+}
