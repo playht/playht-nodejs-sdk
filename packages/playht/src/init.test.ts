8fe6eeacfbdfe87614431a1cca20d133e97531bc
Rodrigo Silveira <rodrigo@play.ht>
2023-09-26 22:04:48 +0100

    Publish

--- packages/playht/src/init.test.ts
+++ packages/playht/src/init.test.ts
@@ -1,10 +1,10 @@
-import { describe, expect, test } from "@jest/globals";
-import * as PlayHT from "./index";
+import { describe, expect, test } from '@jest/globals';
+import * as PlayHT from './index';
 
-describe("exists", () => {
-  test("PlayHT", () => {
+describe('exists', () => {
+  test('PlayHT', () => {
     expect(PlayHT).toBeDefined();
     expect(PlayHT.init).toBeDefined();
-    expect(typeof PlayHT.generateSpeech).toBe("function");
+    expect(typeof PlayHT.generateSpeech).toBe('function');
   });
 });
