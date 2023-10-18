8d29d9f268debdad834658ae4bcf10cd2c4dbf99
Rodrigo Silveira <rodrigo@play.ht>
2023-10-18 12:55:41 +0100

    Remove tsconfig from root eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,7 +16,6 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json'],
   },
   plugins: ['@typescript-eslint'],
   rules: {
