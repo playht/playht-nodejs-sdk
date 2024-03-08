f537ef6f20987f9fd06fa5d242f8d1e6adf873df
mtenpow <3721+mtenpow@users.noreply.github.com>
2024-03-08 12:53:18 -0800

    Make CongestionCtrl enum values explicit

--- packages/playht/src/api/congestionCtrl.ts
+++ packages/playht/src/api/congestionCtrl.ts
@@ -5,14 +5,14 @@ export enum CongestionCtrl {
   /**
    * The client will not do any congestion control.  Text will be sent to PlayHT as fast as possible.
    */
-  Off,
+  Off = 0,
 
   /**
    * The client will optimize for minimizing the number of physical resources required to handle a single stream.
    *
    * If you're using PlayHT On-Prem, you should use this {@link CongestionCtrl} algorithm.
    */
-  StaticMar2024,
+  StaticMar2024 = 1,
 }
 
 /**
