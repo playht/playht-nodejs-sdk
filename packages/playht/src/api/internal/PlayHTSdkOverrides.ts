d5aa4c6cb67c7e641bacdc4e14c8aea90f507cea
Antônio "acdc" Jr. <acdcjunior@gmail.com>
2024-08-14 15:47:38 -0300

    refactor: move v3 config constants to PlayHTSdkOverrides

--- packages/playht/src/api/internal/PlayHTSdkOverrides.ts
+++ packages/playht/src/api/internal/PlayHTSdkOverrides.ts
@@ -7,8 +7,13 @@ import { InferenceCoordinatesEntry } from './v3/v3Overrides';
  * without having to rebuild or modify the SDK itself.
  */
 export const PlayHTSdkOverrides = {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  v3InferenceCoordinatesGenerator: async (userId: string, apiKey: string): Promise<InferenceCoordinatesEntry> => {
-    throw Error('v3InferenceCoordinatesGenerator not initialized');
+  v3: {
+    COORDINATES_EXPIRATION_ADVANCE_REFRESH_TIME_MS: 300_000, // 5 minutes
+    COORDINATES_EXPIRATION_MINIMAL_FREQUENCY_MS: 60_000, // refresh no more frequently than 1 minute
+    COORDINATES_GET_API_CALL_MAX_RETRIES: 3, // number of attempts when calling API to get new coordinates
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    inferenceCoordinatesGenerator: async (userId: string, apiKey: string): Promise<InferenceCoordinatesEntry> => {
+      throw Error('v3.inferenceCoordinatesGenerator not initialized');
+    },
   },
 };
