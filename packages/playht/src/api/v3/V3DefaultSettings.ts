720d1c935e081156bbe6dc042d2bb86382ff6b1e
Antônio "acdc" Jr. <acdcjunior@gmail.com>
2024-10-09 20:44:12 -0300

    refactor: COORDINATES_EXPIRATION_MINIMAL_FREQUENCY_MS -> coordinatesExpirationMinimalFrequencyMs

--- packages/playht/src/api/v3/V3DefaultSettings.ts
+++ packages/playht/src/api/v3/V3DefaultSettings.ts
@@ -4,7 +4,7 @@ export const V3_DEFAULT_SETTINGS = {
   // Try to refresh the coordinates 5 minutes before they expire
   coordinatesExpirationAdvanceRefreshTimeMs: 300_000,
   // Refresh no more frequently than 1 minute
-  COORDINATES_EXPIRATION_MINIMAL_FREQUENCY_MS: 60_000,
+  coordinatesExpirationMinimalFrequencyMs: 60_000,
   // Attempt to get new coordinates 3 times max
   COORDINATES_GET_API_CALL_MAX_RETRIES: 3,
   customInferenceCoordinatesGenerator: undefined,
--- packages/playht/src/api/v3/V3InternalSettings.ts
+++ packages/playht/src/api/v3/V3InternalSettings.ts
@@ -2,7 +2,7 @@ export type V3InternalSettings = {
   // how much time before expiration should we refresh the coordinates
   coordinatesExpirationAdvanceRefreshTimeMs?: number;
   // refresh no more frequently than this
-  COORDINATES_EXPIRATION_MINIMAL_FREQUENCY_MS?: number;
+  coordinatesExpirationMinimalFrequencyMs?: number;
   // number of attempts when calling API to get new coordinates
   COORDINATES_GET_API_CALL_MAX_RETRIES?: number;
   customInferenceCoordinatesGenerator?: (userId: string, apiKey: string) => Promise<InferenceCoordinatesEntry>;
