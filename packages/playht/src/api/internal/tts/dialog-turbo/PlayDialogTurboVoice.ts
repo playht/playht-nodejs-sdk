7e9300fa98df725a40916b55c32f8e57322898af
Antônio "acdc" Jr. <acdcjunior@gmail.com>
2025-03-31 12:04:20 -0300

    fix(PlayDialogTurboVoice): correct syntax for PLAY_DIALOG_TURBO_VOICES_MAP

--- packages/playht/src/api/internal/tts/dialog-turbo/PlayDialogTurboVoice.ts
+++ packages/playht/src/api/internal/tts/dialog-turbo/PlayDialogTurboVoice.ts
@@ -1,6 +1,6 @@
 const PLAY_DIALOG_TURBO_VOICES_MAP: { [key: string]: string } = {
-    "s3://voice-cloning-zero-shot/42c41808-0ddb-4674-8965-024a52ad6c8e/original/manifest.json": "Arista-PlayAI",
-}
+  's3://voice-cloning-zero-shot/42c41808-0ddb-4674-8965-024a52ad6c8e/original/manifest.json': 'Arista-PlayAI',
+};
 
 export function isPlayDialogTurboVoice(voiceId: string): boolean {
   return PLAY_DIALOG_TURBO_VOICES_MAP[voiceId] !== undefined;
