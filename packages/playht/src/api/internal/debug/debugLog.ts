90626fe87711cab5a61b4176616ab415d9702926
Antônio "acdc" Jr. <acdcjunior@gmail.com>
2025-05-05 16:41:11 -0300

    feat(inference): enhance inference address generation with custom retry logic and debug logging

--- packages/playht/src/api/internal/debug/debugLog.ts
+++ packages/playht/src/api/internal/debug/debugLog.ts
@@ -44,3 +44,33 @@ export function logError(
     console.error(`[PlaySDK][${sdkSettings!.userId}] ${errorMessage}`);
   }
 }
+
+export function debugWarn(
+  sdkSettings: Partial<SDKSettings> | undefined,
+  warnMessage: string,
+  logData: Parameters<NonNullable<NonNullable<SDKSettings['debug']>['warn']>>[1],
+) {
+  const debug = sdkSettings?.debug;
+  if (!debug?.enabled) return;
+
+  if (debug.warn) {
+    debug.warn(`[PlaySDK][${sdkSettings!.userId}] ${warnMessage}`, logData);
+  } else {
+    console.warn(`[PlaySDK][${sdkSettings!.userId}] ${warnMessage}`);
+  }
+}
+
+export function debugError(
+  sdkSettings: Partial<SDKSettings> | undefined,
+  errorMessage: string,
+  logData: Parameters<NonNullable<NonNullable<SDKSettings['debug']>['error']>>[1],
+) {
+  const debug = sdkSettings?.debug;
+  if (!debug?.enabled) return;
+
+  if (debug.error) {
+    debug.error(`[PlaySDK][${sdkSettings!.userId}] ${errorMessage}`, logData);
+  } else {
+    console.error(`[PlaySDK][${sdkSettings!.userId}] ${errorMessage}`);
+  }
+}
